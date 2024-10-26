9f5931c1ec3c9437ef8f34a48de441c7bc3c5865
Davina Crahet <davina.crahet@gmail.com>
2024-10-26 05:12:38 +0200

    Contenu et CSS page traiteur OK

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,26 +1,26 @@
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="footer-container">
-                <div className="footer-left">
-                    <NavLink to="/mentions">
-                    <h4>Mentions légales</h4>
-                    </NavLink>
-                    <NavLink to="/about">
-                    <h4>A propos</h4>
-                    </NavLink>
-                    <NavLink to="/contact">
-                    <h4>Contact</h4>
-                    </NavLink>
-                </div>
-                <div className="footer-right">
-                    2024 - Propriété de © Mamacam - Réalisé par Davina Crahet
-                </div>
-            </div>
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <NavLink to="/mentions">
+            <h4>Mentions légales</h4>
+          </NavLink>
+          <NavLink to="/about">
+            <h4>A propos</h4>
+          </NavLink>
+          <NavLink to="/contact">
+            <h4>Contact</h4>
+          </NavLink>
         </div>
-    );
+        <div className="footer-right">
+          2024 - Propriété de © Mamacam - Réalisé par Davina Crahet
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
