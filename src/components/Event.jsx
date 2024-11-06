5ef8b7018c5251a5cea77008eae3e63a4e3a4946
Davina Crahet <davina.crahet@gmail.com>
2024-11-07 00:33:39 +0100

    Events

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,24 @@
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-const Event = () => {
-  return <div></div>;
+const Event = ({ event }) => {
+  return (
+    <div className={event.moment} id="container">
+      <div className="photo">
+        <img src={event.image} alt="Image en lien avec l'évènement" />
+      </div>
+      <div className="content">
+        <h3 className="tag">{event.tag}</h3>
+        <h2 className="title">{event.title}</h2>
+        <h4 className="date">{event.date}</h4>
+        <p className="description">{event.description}</p>
+        <strong className="price">{event.price}</strong>
+      </div>
+    </div>
+  );
 };
 
 export default Event;
+
+Event.propTypes = {
+  event: PropTypes.object.isRequired,
+};
--- src/pages/Evenements.jsx
+++ src/pages/Evenements.jsx
@@ -1,9 +1,14 @@
+import Event from "../components/Event";
+import eventData from "../../public/events.json";
+
 const Evenements = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="timeline">
+      {eventData.map((event, i) => (
+        <Event event={event} key={i} />
+      ))}
+    </div>
+  );
 };
 
 export default Evenements;
