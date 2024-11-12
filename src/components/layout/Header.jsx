67de0d2cc47dd5ae83a7980a9bb8a30c681ed715
Davina Crahet <davina.crahet@gmail.com>
2024-11-12 02:04:31 +0100

    Add mentions

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,48 +4,50 @@ import logo from "../../assets/logo_header_desktop.png";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-nav-left">
-        <NavLink
-          to="/"
-          className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
-        >
-          <h2>Menu</h2>
-        </NavLink>
-        <NavLink
-          to="/traiteur"
-          className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
-        >
-          <h2>Traiteur</h2>
-        </NavLink>
-        <NavLink
-          to="/evenements"
-          className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
-        >
-          <h2>Evènements</h2>
-        </NavLink>
-      </div>
-      <div className="header-logo">
-        <img src={logo} alt="logo Mamacam haut de page" />
-      </div>
-      <div className="header-nav-right">
-        <NavLink
-          to="/about"
-          className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
-        >
-          <h2>Valeurs</h2>
-        </NavLink>
-        <NavLink
-          to="/partenaires"
-          className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
-        >
-          <h2>Partenaires</h2>
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
-        >
-          <h2>Contact</h2>
-        </NavLink>
+      <div className="header-container">
+        <div className="header-nav-left">
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
+          >
+            <h2>Menu</h2>
+          </NavLink>
+          <NavLink
+            to="/traiteur"
+            className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
+          >
+            <h2>Traiteur</h2>
+          </NavLink>
+          <NavLink
+            to="/evenements"
+            className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
+          >
+            <h2>Evènements</h2>
+          </NavLink>
+        </div>
+        <div className="header-logo">
+          <img src={logo} alt="logo Mamacam haut de page" />
+        </div>
+        <div className="header-nav-right">
+          <NavLink
+            to="/about"
+            className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
+          >
+            <h2>Valeurs</h2>
+          </NavLink>
+          <NavLink
+            to="/partenaires"
+            className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
+          >
+            <h2>Partenaires</h2>
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}
+          >
+            <h2>Contact</h2>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/Mentions.jsx
@@ -0,0 +1,9 @@
+const Mentions = () => {
+  return (
+    <div className="mentions">
+      <h1>Mentions légales</h1>
+    </div>
+  );
+};
+
+export default Mentions;
