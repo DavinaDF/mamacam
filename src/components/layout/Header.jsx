665632fa4e0a9a7d241ddd9dc6fc005fc66bbb5c
Davina Crahet <davina.crahet@gmail.com>
2024-12-08 23:02:34 +0100

    Navigation responsive

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -63,9 +63,12 @@
 // export default Header;
 
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import logo from "../../assets/logo_header_desktop.png";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="header">
       <div className="header-container">
@@ -73,27 +76,32 @@ const Header = () => {
           <div className="logo">
             <img src={logo} alt="logo Mamacam haut de page" />
           </div>
-          <i className="fa-solid fa-bars menu-button"></i>
-          <div className="menu-links close">
-            <NavLink to="/" className="nav-mobile-item">
-              <h2>Menu</h2>
-            </NavLink>
-            <NavLink to="/traiteur" className="nav-mobile-item">
-              <h2>Traiteur</h2>
-            </NavLink>
-            <NavLink to="/evenements" className="nav-mobile-item">
-              <h2>Evènements</h2>
-            </NavLink>
-            <NavLink to="/about" className="nav-mobile-item">
-              <h2>Valeurs</h2>
-            </NavLink>
-            <NavLink to="/partenaires" className="nav-mobile-item">
-              <h2>Partenaires</h2>
-            </NavLink>
-            <NavLink to="/contact" className="nav-mobile-item">
-              <h2>Contact</h2>
-            </NavLink>
-          </div>
+          <i
+            className="fa-solid fa-bars menu-button"
+            onClick={() => setIsOpen(!isOpen)}
+          ></i>
+          {isOpen && (
+            <div className="menu-links" onClick={() => setIsOpen(!isOpen)}>
+              <NavLink to="/" className="nav-mobile-item">
+                <h2>Menu</h2>
+              </NavLink>
+              <NavLink to="/traiteur" className="nav-mobile-item">
+                <h2>Traiteur</h2>
+              </NavLink>
+              <NavLink to="/evenements" className="nav-mobile-item">
+                <h2>Evènements</h2>
+              </NavLink>
+              <NavLink to="/about" className="nav-mobile-item">
+                <h2>Valeurs</h2>
+              </NavLink>
+              <NavLink to="/partenaires" className="nav-mobile-item">
+                <h2>Partenaires</h2>
+              </NavLink>
+              <NavLink to="/contact" className="nav-mobile-item">
+                <h2>Contact</h2>
+              </NavLink>
+            </div>
+          )}
         </div>
         <div className="nav-desktop">
           <div className="header-nav-left">
